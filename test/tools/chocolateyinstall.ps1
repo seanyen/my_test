144b7f6df1a27062273842ec60bfa1b8aff2d0d2
Sean Yen <seanyen@microsoft.com>
2020-06-04 15:44:48 -0700

    Update chocolateyinstall.ps1

--- test/tools/chocolateyinstall.ps1
+++ test/tools/chocolateyinstall.ps1
@@ -1,5 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+Write-Warning "size is $Env:PATH.length"
+
 $vcpkgInstall = Join-Path $toolsDir 'vcpkgInstall.ps1'
 Invoke-Expression "$vcpkgInstall"
