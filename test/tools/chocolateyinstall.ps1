ddfd70662f8baabb3109368c558ac2dd8b798ceb
Sean Yen <seanyen@microsoft.com>
2020-06-04 17:25:28 -0700

    Update chocolateyinstall.ps1

--- test/tools/chocolateyinstall.ps1
+++ test/tools/chocolateyinstall.ps1
@@ -2,6 +2,13 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $PathLen = $Env:PATH.length
+if ($PathLen -gt 4096) {
+    Write-Warning "The environment variable of %PATH% is over 4096."
+    Write-Warning "This could potentially break the installation."
+    Write-Warning "Please reduce the length and retry again."
+    throw "%PATH% is too long."
+}
+
 Write-Warning "size is $PathLen"
 
 $vcpkgInstall = Join-Path $toolsDir 'vcpkgInstall.ps1'
